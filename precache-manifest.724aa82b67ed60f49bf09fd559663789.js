9731fc276a079898ccd45a1200a6394d317961ca
Vijay <vijay@pixels.agency>
2019-12-19 11:01:40 +0530

    Updates

---
+++ precache-manifest.724aa82b67ed60f49bf09fd559663789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3beff38977d18de1bd1fa9ef7b07395b",
+    "url": "/spacemission/index.html"
+  },
+  {
+    "revision": "ec584550eed0ae8eb647",
+    "url": "/spacemission/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0f5ab30621b221151e56",
+    "url": "/spacemission/static/css/main.7457d8a9.chunk.css"
+  },
+  {
+    "revision": "ec584550eed0ae8eb647",
+    "url": "/spacemission/static/js/2.76cc24de.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/spacemission/static/js/2.76cc24de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f5ab30621b221151e56",
+    "url": "/spacemission/static/js/main.50c7d166.chunk.js"
+  },
+  {
+    "revision": "3dbb15cb98a4832a13bf",
+    "url": "/spacemission/static/js/runtime-main.6b12f6b7.js"
+  }
+]);
